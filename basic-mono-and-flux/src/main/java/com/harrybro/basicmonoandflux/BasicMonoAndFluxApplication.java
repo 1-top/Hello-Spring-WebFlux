a14a06c232a2aaa6ff00451c8ebba694cb88e1a6
Harry-Bro <likeharrybro@gmail.com>
2021-09-12 01:05:21 +0900

    :tada: Initialize Project

---
+++ basic-mono-and-flux/src/main/java/com/harrybro/basicmonoandflux/BasicMonoAndFluxApplication.java
@@ -0,0 +1,13 @@
+package com.harrybro.basicmonoandflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicMonoAndFluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicMonoAndFluxApplication.class, args);
+    }
+
+}
---
+++ basic-mono-and-flux/src/test/java/com/harrybro/basicmonoandflux/BasicMonoAndFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harrybro.basicmonoandflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicMonoAndFluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
