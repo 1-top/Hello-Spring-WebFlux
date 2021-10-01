99a19850c4fb304440c845eac88a3831524923c6
Harry-Bro <likeharrybro@gmail.com>
2021-10-01 23:29:31 +0900

    :fire: Remove unnecessary nullable parameter

--- hello-kotlin-with-webflux/src/main/kotlin/com/harrybro/hellokotlinwithwebflux/domain/product/dao/ProductRepository.kt
+++ hello-kotlin-with-webflux/src/main/kotlin/com/harrybro/hellokotlinwithwebflux/domain/product/dao/ProductRepository.kt
@@ -4,7 +4,6 @@ import com.harrybro.hellokotlinwithwebflux.domain.product.domain.Product
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository
 import reactor.core.publisher.Flux
 
-//@Repository
 interface ProductRepository: ReactiveMongoRepository<Product, String> {
     fun findAllByPriceBetween(min: Double, max: Double): Flux<Product>
 }
