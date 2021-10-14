8944eaef4b694379cb9944f2fb9041b2e2156926
Harry-Bro <likeharrybro@gmail.com>
2021-10-15 06:54:33 +0900

    :tada: Initialize Project

---
+++ kotlin-basic-ecommerce/src/main/kotlin/com/harrybro/kotlinbasicecommerce/KotlinBasicEcommerceApplication.kt
@@ -0,0 +1,11 @@
+package com.harrybro.kotlinbasicecommerce
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinBasicEcommerceApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinBasicEcommerceApplication>(*args)
+}
---
+++ kotlin-basic-ecommerce/src/test/kotlin/com/harrybro/kotlinbasicecommerce/KotlinBasicEcommerceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.harrybro.kotlinbasicecommerce
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinBasicEcommerceApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
